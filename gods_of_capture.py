815865e11c5c1cf1790da81020cedc98326aefdd
Elepert <emily.lepert@students.olin.edu>
2017-03-27 22:50:23 -0400

    Updated base class with pseudocode comments

--- gods_of_capture.py
+++ gods_of_capture.py
@@ -46,6 +46,12 @@ class View():
 
 class Controller():
     """Holds functions for manipulating the model"""
+    def generate_new_unit(time, unit_type):
+        #for each team, if time = 5s
+            #new_unit = Unit(x,y,team) => x, y would be set for each team
+            #new_unit.draw(x,y)
+            #team_base.unit_generation()
+        pass
 
 
 class Unit():  # TODO Make uninstantiable
@@ -86,12 +92,32 @@ class Flag():
 
 class Base():
     """ The base class for the game"""
-    def __init__(self):
+    def __init__(self, x, y, color, current_time):
         # TODO: Initialize attributes like position, type of unit selected
-        pass
+        self.position = x, y #pixel position (idk if it's center or corner)
+            # would need to see about pygame shapes
+        self.color = color # pygame command (imagine that this would change depending
+            # on the type of unit being produced or could be a time indicator)
+        self.unit_type = unit_type #this is just a placeholder, I imagine that
+            # we'd pass this into a fxn or something like that rather than have it
+            # be an attribute
+        self.time = current_time # what time is it in the game?
+            # i'm imagigining that theres a time module in python so this would
+            # keep track of the seconds (ie: is it 5s or 6s)
+            
+        
 
     def draw(self):
         # TODO draw sprite at location
+
+        pass
+
+    def change_color():
+        # change the self.color depending on the unit type
+        pass
+
+    def unit_generation():
+        # when a unit is generated, have some visual effect
         pass
 
     # TODO more methods here!
