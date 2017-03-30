36dcb55de416a459ddc4b27c687d243b398ae4f6
Connor Novak <connor.r.novak@gmail.com>
2017-03-30 13:48:13 -0400

    Added some stuff!

--- gods_of_capture.py
+++ gods_of_capture.py
@@ -15,45 +15,76 @@ class CaptureGame():
         pygame.init()                   # initialize pygame
         self.screen_size = [1840, 920]  # size of screen
         self.screen = pygame.display.set_mode(self.screen_size)
+        # Add background sprite
+
+        # Initialize MVC classes
         self.model = Model()
-        self.view = View()
-        self.control = Control()
+        self.view = View(self.model)
+        self.control = Controller(self.model)
+
         self.running = True
 
     def run(self):
-        while self.run:
+        while self.running:
             """runs the game loop"""
             # TODO Check wincase (Controller)
             for event in pygame.event.get():
-                if event.type == pygame.QUIT:  # quits
+                if event.type == pygame.QUIT or event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:  # quits
                     self.running = False
-            # TODO Escape key
             # User Input May Eventually go here
             # AI Input WILL Go here
 
 
-class Model():
+class Model(object):
     """Makes all starter objects,
     - Field, Bases, Flags, Units..."""
 
+    def __init__(self):
+        '''DOCSTRING
+            Initializes Model for initial game stages
+            '''
 
-class View():
+
+
+class View(object):
     """Makes the draw methods for all of the classes with"""
 
-    def main.unit.draw():
-        pass
+    def __init__(self,model):
+        """DOCSTRING
+            Initializes View object to allow references to model"""
+        self.model = model
 
 
-class Controller():
-    """Holds functions for manipulating the model"""
+    def draw(self, surface):
+        surface.blit(self.model.icon, (self.model.x, self.model.y))
+
+
+class Controller(object):
+    """DOCSTRING
+        Holds functions for manipulating the model
+        """
+
+    def __init__(self, model):
+        """DOCSTRING
+            Initializes Controller object to allow manipulation of Model
+            """
+
+        self.model = model
+
 
 
 class Unit():  # TODO Make uninstantiable
 
-    def __init__(self, x, y, team):  # TODO set to position of the base
+    def __init__(self, x, y, team, stats,sprite):  # TODO set to position of the base
         self.position = x, y
         self.team = team
         self.is_selected = False
+        self.strength = stats[0]
+        self.speed = stats[1]
+        self.health = stats[2]
+        self.attack = stats[3]
+        self.cooldown = stats[4]
+        self.sprite = sprite
 
     def draw():
         pass
@@ -62,14 +93,21 @@ class Unit():  # TODO Make uninstantiable
 # Example specific unit for later use
 class Teenie(Unit):
     """ The base unit in the game"""
-    def __init__(self, strength=4, speed=8, health=12, attack=2, cooldown=3):
+    def __init__(self, x, y, team):
+        Unit.__init__(self, x, y, team, [5,6,10,2,2],sprite)
+
+class Speedie(Unit):
+    """ The fast unit in the game"""
+    def __init__(self, strength=4, speed = 9, health = 12, attack=1, cooldown=1):
         self.strength = strength
         self.speed = speed
         self.health = health
         self.attack = attack
         self.cooldown = cooldown
-        self.sprite = sprite  # TODO: put Teenie sprite here
+        self.sprite = sptire # TODO: put Speedie sprite here
 
+class Heavie(Unit):
+    """The strong unit in the game"""
 
 class Flag():
     """ The flag class for the game"""
