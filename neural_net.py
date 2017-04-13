3a62e2e86d6fffccd63d4938e176322b4def8233
Elepert <emily.lepert@students.olin.edu>
2017-04-13 14:17:03 -0400

    still figuring out neural nets

--- neural_net.py
+++ neural_net.py
@@ -36,7 +36,7 @@ def dsigmoid(y, deriv=True):
 	return(y * (1 - y))
 
 def nonlin(x,deriv=False):
-	if deriv == True:
+	if (deriv == True):
 		return(x*(1-x))
 
 	return(1/(1+np.exp(-x)))
@@ -55,21 +55,21 @@ syn1 = 50*np.random.random((5,12))-1
 
 for j in range(50000):
 	l0 = X
-	l1 = dsigmoid(np.dot(l0,syn0))
-	l2 = dsigmoid(np.dot(l1,syn1))
+	l1 = nonlin(np.dot(l0,syn0))
+	l2 = nonlin(np.dot(l1,syn1))
 
 	l2_error = Y - l2
 
 	if(j % 10000) == 12:
 		print("Error: " + str(np.mean(np.abs(l2_error))))
 
-	l2_delta = l2_error*dsigmoid(l2, deriv=True)
+	l2_delta = l2_error*nonlin(l2, deriv=True)
 	l1_error = l2_delta.dot(syn1.T)
-	l1_delta = l1_error*dsigmoid(l1,deriv=True)
+	l1_delta = l1_error*nonlin(l1,deriv=True)
 
 	if(j % 10000) == 12:
 		#print("Error: " + str(np.mean(np.abs(l2_error))))
-		print("syn1 old: " + str(sigmoid((np.dot(l0,syn0)))))
+		print("syn1 old: " + str(nonlin((np.dot(l0,syn0)))))
 	syn1 += l1.T.dot(l2_delta)
 	syn0 += l0.T.dot(l1_delta)
 
