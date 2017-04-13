ac2383c81556a95492734304510e2dfb944a6656
Elepert <emily.lepert@students.olin.edu>
2017-04-13 14:03:07 -0400

    updated changes

--- neural_net.py
+++ neural_net.py
@@ -18,38 +18,50 @@ information_sample2 = [[screen_x-1300, screen_y - 550, 1, 1, 1], [screen_x-1500,
 def single_list(lists):
 	return([item for sublist in lists for item in sublist])
 
-def nonlin(x,deriv=False):
-	if(deriv==True):
-		return x*(1-x)
+def sigmoid(x):
+    return(1/(1 + np.exp(-x)))
+# derivative of sigmoid
+# sigmoid(y) * (1.0 - sigmoid(y))
+# the way we use this y is already sigmoided
+def dsigmoid(y, deriv=True):
+	y = sigmoid(y)
+	return(y * (1 - y))
 
-	return 1/(1+np.exp(-x))
+def nonlin(x,deriv=False):
+	if deriv == True:
+		return(x*(1-x))
+
+	return(1/(1+np.exp(-x)))
 
 #input data
 X = np.array([single_list(information_sample1),
 	single_list(information_sample2)])
 
-y = np.array([calc.sum_forces_all(information_sample1),
+Y = np.array([calc.sum_forces_all(information_sample1),
 	calc.sum_forces_all(information_sample2)])
-
+print(Y)
 np.random.seed(1)
 
-syn0 = 2*np.random.random((40,4)) -1 
-syn1 = 2*np.random.random((4,12)) -1
+syn0 = 50*np.random.random((40,5))-1 
+syn1 = 50*np.random.random((5,12))-1
 
 for j in range(50000):
 	l0 = X
-	l1 = nonlin(np.dot(l0,syn0))
-	l2 = nonlin(np.dot(l1,syn1))
+	l1 = dsigmoid(np.dot(l0,syn0))
+	l2 = dsigmoid(np.dot(l1,syn1))
 
-	l2_error = y - l2
+	l2_error = Y - l2
 
 	if(j % 10000) == 12:
 		print("Error: " + str(np.mean(np.abs(l2_error))))
 
-	l2_delta = l2_error*nonlin(l2, deriv=True)
+	l2_delta = l2_error*dsigmoid(l2, deriv=True)
 	l1_error = l2_delta.dot(syn1.T)
-	l1_delta = l1_error*nonlin(l1,deriv=True)
+	l1_delta = l1_error*dsigmoid(l1,deriv=True)
 
+	if(j % 10000) == 12:
+		#print("Error: " + str(np.mean(np.abs(l2_error))))
+		print("syn1 old: " + str(sigmoid((np.dot(l0,syn0)))))
 	syn1 += l1.T.dot(l2_delta)
 	syn0 += l0.T.dot(l1_delta)
 
